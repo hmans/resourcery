2349cf683c73c9a687e96963470ff51f56362554
Hendrik Mans <hendrik@mans.de>
2012-12-19 20:21:10 +0100

    Changed serve_resource to accept resource class as its first parameter.

--- lib/resourcery/controller_macro.rb
+++ lib/resourcery/controller_macro.rb
@@ -1,15 +1,15 @@
 module Resourcery
   module ControllerMacro
-    def serve_resource(opts = {})
+    def serve_resource(klass = nil, opts = {})
       @resource_options = {
         finder: :find,
-        class: name.gsub(/Controller$/, '').singularize.constantize
+        klass: klass || name.gsub(/Controller$/, '').singularize.constantize
       }.merge(opts)
 
       extend  ControllerExtensions::ClassMethods
       include ControllerExtensions::InstanceMethods
 
-      @resource_class = @resource_options[:class]
+      @resource_class = @resource_options[:klass]
 
       helper_method :resource, :collection
     end
